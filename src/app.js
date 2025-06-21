62b6cc19177b505855c143041df164ed451070c0
dazer-m-l <dazer.mc1397@gmail.com>
2025-06-20 19:51:32 -0600

    [ADD, Refactor]: Agregando codificacion al app.js y Creacion de Archivo PDF

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const scrapeArticles = require('./js/script');
 const exportarXLSX = require('./js/exportarXLSX');
+const exportarPDF = require('./js/exportarPDF');
 
 (async () => {
   const articles = await scrapeArticles();
   exportarXLSX(articles);
+  exportarPDF(articles)
 })();
